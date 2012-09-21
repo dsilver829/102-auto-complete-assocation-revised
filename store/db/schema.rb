2dc625fd039cb6aefb6a6a08993a54bf14f2a162
David Silver <dsilver@candidatemetrics.com>
2012-09-21 08:20:35 -0700

    setting up

--- store/db/schema.rb
+++ store/db/schema.rb
@@ -15,16 +15,16 @@ ActiveRecord::Schema.define(:version => 20111110182430) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "products", :force => true do |t|
     t.string   "name"
     t.decimal  "price"
     t.integer  "category_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
 end
